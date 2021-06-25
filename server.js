980c326e3ddae6a9bd83e69ed5dd1fb9248a58fe
Megan Steblay <megan@steblay.net>
2021-06-25 16:48:36 -0500

    Updating routes for HTML

--- server.js
+++ server.js
@@ -7,8 +7,10 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, () => {
     console.log(`App listening on PORT: ${PORT}`);
-  });
+});
   
