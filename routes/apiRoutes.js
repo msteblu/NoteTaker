2202657eb3f81d65d83a095177c35603ca53918d
Megan Steblay <megan@steblay.net>
2021-06-27 17:54:21 -0500

    Getting routes to work

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,16 +1,37 @@
-// LOAD DATA
-const data = require('../db/db.json');
+// IMPORT FILESYSTEM
+const fs = require('fs');
+
+// DATA
+let notesData = {};
 
 // ROUTING 
 
 module.exports = (app) => {
     // API GET Requests
-    app.get('/api/notes', (req, res) => res.json(data));
-  
+    app.get('/api/notes', (req, res) => {
+        fs.readFile(`${__dirname}/../db/db.json`, (err, data) => {
+            if (err) {
+                console.error(err);
+                return;
+            } else {
+                notesData = JSON.parse(data);
+                res.json(notesData);
+            }
+        });
+    });
+
     // API POST Requests
     app.post('/api/notes', (req, res) => {
+        notesData.push(req.body);
 
+        fs.writeFile(`${__dirname}/../db/db.json`, JSON.stringify(notesData), (err) => {
+            if (err) {
+                console.log('Error writing file', err)
+            } else {
+                console.log('Successfully wrote file')
+            }
+        })
+        res.json(req.body);
     });
-  
-    //should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+
 };
